16fae235e515910c6458cd38ce7be1f3854a5114
Gampyal <gampyal07@gmail.com>
2023-04-28 00:20:37 +0100

    03

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('index', [JobController::class, 'index']);
 Route::post('store', [JobController::class, 'store']);
-Route::get('show/{$job}', [JobController::class, 'show']);
-Route::post('update/{$job}', [JobController::class, 'update']);
-Route::post('delete/{$job}', [JobController::class, 'delete']);
+Route::get('show/{job}', [JobController::class, 'show']);
+Route::post('update/{job}', [JobController::class, 'update']);
+Route::post('delete/{job}', [JobController::class, 'delete']);
